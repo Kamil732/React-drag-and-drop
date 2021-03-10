e4d9776c6a7b70228b0e34dc611d42142aec4e42
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-10 19:45:45 +0100

    initial commit

---
+++ to-do/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Board from './components/Board'
+
+function App() {
+	return (
+		<div className="app">
+			<Board>LMAO</Board>tyku
+		</div>
+	)
+}
+
+export default App
