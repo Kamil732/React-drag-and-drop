26e36be0f136fccb9b61ef54b1492549108f31f1
Kamil Głuchowski <kamilkamil5542@wp.pl>
2021-03-10 21:44:33 +0100

    added drag & drop in react beautiful dnd

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,34 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import main from './assets/css/main.css'
 
-import Board from './components/Board'
-import Card from './components/Card'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const finalCharacters = [
+	{
+		id: 'gary',
+		name: 'Gary Goodspeed',
+	},
+	{
+		id: 'cato',
+		name: 'Little Cato',
+	},
+	{
+		id: 'kvn',
+		name: 'KVN',
+	},
+	{
+		id: 'mooncake',
+		name: 'Mooncake',
+	},
+	{
+		id: 'quinn',
+		name: 'Quinn Ergon',
+	},
+]
 
 function App() {
+	const [characters, setCharacters] = useState(finalCharacters)
+
+	const handleOnDragEnd = (result) => {
+		if (!result.destination) return
+
+		const items = Array.from(characters)
+		const [reorderedItem] = items.splice(result.source.index, 1)
+		items.splice(result.destination.index, 0, reorderedItem)
+
+		setCharacters(items)
+	}
+
 	return (
-		<div
-			className="flexbox"
-			onDrop={(e) => {
-				e.preventDefault()
-				const card_id = e.dataTransfer.getData('card_id')
-				const card = document.getElementById(card_id)
-				card.style.visibility = 'visible'
-			}}
-			onDragOver={(e) => e.preventDefault()}
-		>
-			<Board id="board-1" className="board">
-				<Card id="card-1" className="card" draggable="true">
-					<p>Card 1</p>
-				</Card>
-				<Card id="card-2" className="card" draggable="true">
-					<p>Card 2</p>
-				</Card>
-			</Board>
-			<Board id="board-2" className="board">
-				<Card id="card-3" className="card" draggable="true">
-					<p>Card 3</p>
-				</Card>
-			</Board>
+		<div className="app">
+			<header className="app-header">
+				<h1>Final Space Characters</h1>
+				<DragDropContext onDragEnd={handleOnDragEnd}>
+					<Droppable droppableId="characters">
+						{(provided) => (
+							<ul
+								className="characters"
+								{...provided.droppableProps}
+								ref={provided.innerRef}
+							>
+								{characters.map(({ id, name }, index) => (
+									<Draggable
+										key={id}
+										draggableId={id}
+										index={index}
+									>
+										{(provided) => (
+											<li
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												ref={provided.innerRef}
+											>
+												<p>{name}</p>
+											</li>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</ul>
+						)}
+					</Droppable>
+				</DragDropContext>
+			</header>
 		</div>
 	)
 }
